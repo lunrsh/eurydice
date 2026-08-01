350c1c50c715dc6c05b07e40eb54b3e276c5705d
Luna <luna@lunr.sh>
2026-07-31 19:11:30 -0500

    chore: Add scripts to compile FFmpeg for Windows

---
+++ ci/build_ffmpeg.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+set -x
+export CC="zig cc -target x86_64-windows-gnu"
+export CXX="zig c++ -target x86_64-windows-gnu"
+export LD="zig cc -target x86_64-windows-gnu"
+
+mkdir -p /tmp/ffmpeg /tmp/libmp3lame
+pushd /tmp/libmp3lame
+curl -L https://sourceforge.net/projects/lame/files/lame/3.100/lame-3.100.tar.gz > lame.tar.gz
+tar -xzvf lame.tar.gz .
+cd lame-3.100
+./configure --host=x86_64-w64-mingw32
+make -j$(nproc)
+sudo make install # TODO: unsafe, migrate to prefix soon
+popd
+
+git clone https://git.ffmpeg.org/ffmpeg.git /tmp/ffmpeg -b n8.1.2
+pushd /tmp/ffmpeg
+./configure --arch=x86_64 --target-os=mingw32 --cross-prefix=x86_64-w64-mingw32- --ld="$LD" --disable-programs --enable-gpl --enable-libmp3lame --enable-nonfree --enable-static --disable-shared
+make
+sudo make install # TODO: unsafe, migrate to prefix soon
+popd
