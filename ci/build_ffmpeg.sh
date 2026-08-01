cbf1173d8a3eaf008b45f63c1142cf51129386e1
Luna <luna@lunr.sh>
2026-07-31 22:42:33 -0500

    fix: Fixes compiler failures due to dynamic linkage

--- ci/build_ffmpeg.sh
+++ ci/build_ffmpeg.sh
@@ -11,14 +11,14 @@ pushd /tmp/libmp3lame
 curl -L https://sourceforge.net/projects/lame/files/lame/3.100/lame-3.100.tar.gz > lame.tar.gz
 tar -xzvf lame.tar.gz
 cd lame-3.100
-./configure --host=x86_64-w64-mingw32 CC="$CC" CXX="$CXX" LD="$LD" AR="$AR" RANLIB="zig ranlib"
+./configure --host=x86_64-w64-mingw32 --disable-shared --enable-static --cc="$CC" --cxx="$CXX" --ld="$LD"
 make -j$(nproc)
 sudo make install # TODO: unsafe, migrate to prefix soon
 popd
 
 git clone https://git.ffmpeg.org/ffmpeg.git /tmp/ffmpeg -b n8.1.2
 pushd /tmp/ffmpeg
-./configure --arch=x86_64 --target-os=mingw32 --cross-prefix=x86_64-w64-mingw32- -cc="$CC" --cxx="$CXX" --ld="$LD" --disable-programs --enable-gpl --enable-libmp3lame --enable-nonfree --enable-static --disable-shared
+./configure --arch=x86_64 --target-os=mingw32 --cross-prefix=x86_64-w64-mingw32- --cc="$CC" --cxx="$CXX" --ld="$LD" --disable-programs --enable-gpl --enable-libmp3lame --enable-nonfree --enable-static --disable-shared
 make
 sudo make install # TODO: unsafe, migrate to prefix soon
 popd
