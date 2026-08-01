42cc932e3818681d454f6184a9471ac097523e9b
Luna <luna@lunr.sh>
2026-07-31 21:42:37 -0500

    fix: Fixes ffmpeg using wrong AR package

--- ci/build_ffmpeg.sh
+++ ci/build_ffmpeg.sh
@@ -11,7 +11,7 @@ pushd /tmp/libmp3lame
 curl -L https://sourceforge.net/projects/lame/files/lame/3.100/lame-3.100.tar.gz > lame.tar.gz
 tar -xzvf lame.tar.gz
 cd lame-3.100
-./configure --host=x86_64-w64-mingw32
+./configure --host=x86_64-w64-mingw32 CC="$CC" CXX="$CXX" LD="$LD" AR="$AR" RANLIB="zig ranlib"
 make -j$(nproc)
 sudo make install # TODO: unsafe, migrate to prefix soon
 popd
