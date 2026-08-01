15e63007da178a127115759bbeeeef04a1ded492
Luna <luna@lunr.sh>
2026-07-31 19:22:56 -0500

    fix: Fixes ffmpeg and lame failing to build

--- ci/build_ffmpeg.sh
+++ ci/build_ffmpeg.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-set -x
+set -xeuo pipefail
+
 export CC="zig cc -target x86_64-windows-gnu"
 export CXX="zig c++ -target x86_64-windows-gnu"
 export LD="zig cc -target x86_64-windows-gnu"
@@ -7,16 +8,16 @@ export LD="zig cc -target x86_64-windows-gnu"
 mkdir -p /tmp/ffmpeg /tmp/libmp3lame
 pushd /tmp/libmp3lame
 curl -L https://sourceforge.net/projects/lame/files/lame/3.100/lame-3.100.tar.gz > lame.tar.gz
-tar -xzvf lame.tar.gz .
+tar -xzvf lame.tar.gz
 cd lame-3.100
-./configure --host=x86_64-w64-mingw32
+./configure --host=x86_64-w64-mingw32 -cc="$CC" --cxx="$CXX" --ld="$LD"
 make -j$(nproc)
 sudo make install # TODO: unsafe, migrate to prefix soon
 popd
 
 git clone https://git.ffmpeg.org/ffmpeg.git /tmp/ffmpeg -b n8.1.2
 pushd /tmp/ffmpeg
-./configure --arch=x86_64 --target-os=mingw32 --cross-prefix=x86_64-w64-mingw32- --ld="$LD" --disable-programs --enable-gpl --enable-libmp3lame --enable-nonfree --enable-static --disable-shared
+./configure --arch=x86_64 --target-os=mingw32 --cross-prefix=x86_64-w64-mingw32- -cc="$CC" --cxx="$CXX" --ld="$LD" --disable-programs --enable-gpl --enable-libmp3lame --enable-nonfree --enable-static --disable-shared
 make
 sudo make install # TODO: unsafe, migrate to prefix soon
 popd
