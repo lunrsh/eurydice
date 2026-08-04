ae271c24d068072dfc12c7c98b79084d2dbffcf0
Luna <luna@lunr.sh>
2026-08-03 21:46:20 -0400

    fix: Switch lame back to 3.100

--- ci/build_ffmpeg.sh
+++ ci/build_ffmpeg.sh
@@ -10,7 +10,7 @@ export AR="zig ar"
 mkdir -p /tmp/ffmpeg /tmp/libmp3lame /tmp/ffmpeg_dist
 
 pushd /tmp/libmp3lame
-curl -L https://sourceforge.net/projects/lame/files/lame/3.101/lame-3.101.tar.gz > lame.tar.gz
+curl -L https://sourceforge.net/projects/lame/files/lame/3.100/lame-3.100.tar.gz > lame.tar.gz
 tar -xzvf lame.tar.gz
 cd lame-3.100
 ./configure --host=x86_64-w64-mingw32 --prefix=/tmp/ffmpeg_dist --disable-shared --enable-static --disable-frontend --disable-dependency-tracking --disable-decoder
