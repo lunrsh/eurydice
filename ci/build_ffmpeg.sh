aaa247e995779b54d828cb8b725620075f4dd167
Luna <luna@lunr.sh>
2026-08-03 20:50:32 -0400

    fix: Makes ffmpeg_dist directory the prefix path to fix detection

--- ci/build_ffmpeg.sh
+++ ci/build_ffmpeg.sh
@@ -6,13 +6,13 @@ export CXX="zig c++ -target x86_64-windows-gnu"
 export LD="zig cc -target x86_64-windows-gnu"
 export AR="zig ar"
 
-mkdir -p /tmp/ffmpeg /tmp/libmp3lame
+mkdir -p /tmp/ffmpeg /tmp/libmp3lame /tmp/ffmpeg_dist
 
 pushd /tmp/libmp3lame
 curl -L https://sourceforge.net/projects/lame/files/lame/3.100/lame-3.100.tar.gz > lame.tar.gz
 tar -xzvf lame.tar.gz
 cd lame-3.100
-./configure --host=x86_64-w64-mingw32 --disable-shared --enable-static --disable-frontend --disable-dependency-tracking --disable-decoder
+./configure --host=x86_64-w64-mingw32 --prefix=/tmp/ffmpeg_dist --disable-shared --enable-static --disable-frontend --disable-dependency-tracking --disable-decoder
 sed -i \
   's|old_archive_cmds="lib -OUT:\\$oldlib\\$oldobjs\\$old_deplibs"|old_archive_cmds="\\$AR rcs \\$oldlib \\$oldobjs"|' \
   libtool
@@ -22,7 +22,7 @@ popd
 
 git clone https://git.ffmpeg.org/ffmpeg.git /tmp/ffmpeg -b n8.1.2
 pushd /tmp/ffmpeg
-./configure --arch=x86_64 --target-os=mingw32 --cross-prefix=x86_64-w64-mingw32- --cc="$CC" --cxx="$CXX" --ld="$LD" --disable-programs --enable-gpl --enable-libmp3lame --enable-nonfree --enable-static --disable-shared
+./configure --arch=x86_64 --target-os=mingw32 --prefix=/tmp/ffmpeg_dist --cross-prefix=x86_64-w64-mingw32- --cc="$CC" --cxx="$CXX" --ld="$LD" --disable-programs --enable-gpl --enable-libmp3lame --enable-nonfree --enable-static --disable-shared
 make
 sudo make install # TODO: unsafe, migrate to prefix soon
 popd
