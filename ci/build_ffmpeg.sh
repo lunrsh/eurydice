081e3289e4751a6201c16364e84f698b3d68b3e6
Luna <luna@lunr.sh>
2026-09-09 19:37:05 -0400

    fix: Makes ffmpeg build more compact

--- ci/build_ffmpeg.sh
+++ ci/build_ffmpeg.sh
@@ -24,7 +24,112 @@ popd
 
 git clone https://git.ffmpeg.org/ffmpeg.git /tmp/ffmpeg -b n8.1.2
 pushd /tmp/ffmpeg
-./configure --arch=x86_64 --target-os=mingw32 --prefix=/tmp/ffmpeg_dist --cross-prefix=x86_64-w64-mingw32- --cc="$CC" --cxx="$CXX" --ld="$LD" --extra-cflags="-I/tmp/ffmpeg_dist/include" --extra-ldflags="-L/tmp/ffmpeg_dist/lib" --disable-programs --enable-gpl --enable-libmp3lame --enable-nonfree --enable-static --disable-shared
+# from https://github.com/mcmtroffaes/ffmpeg-msvc-build/issues/5
+./configure --arch=x86_64 --target-os=mingw32 --prefix=/tmp/ffmpeg_dist --cross-prefix=x86_64-w64-mingw32- --cc="$CC" --cxx="$CXX" --ld="$LD" --extra-cflags="-I/tmp/ffmpeg_dist/include" --extra-ldflags="-L/tmp/ffmpeg_dist/lib" \
+	--disable-shared \
+	--enable-static \
+	--disable-debug \
+	--disable-programs \
+	--enable-gpl \
+	--enable-nonfree \
+	--disable-doc \
+	--disable-avdevice \
+	--disable-swscale \
+	--disable-postproc \
+	--disable-ffmpeg \
+	--disable-ffplay \
+	--disable-iconv \
+	--disable-zlib \
+	--disable-bzlib \
+	--disable-lzma \
+	--disable-sdl2 \
+	--disable-schannel \
+	--disable-securetransport \
+	--disable-xlib \
+	--disable-muxers \
+	--disable-demuxers \
+	--disable-hwaccels \
+	--disable-d3d11va \
+	--disable-nvenc \
+	--disable-dxva2 \
+	--disable-bsfs \
+	--disable-filters \
+	--disable-parsers \
+	--disable-indevs \
+	--disable-outdevs \
+	--disable-encoders \
+	--disable-decoders \
+	--enable-libmp3lame \
+	--enable-encoder=libmp3lame \
+	--enable-encoder=flac \
+	--enable-encoder=alac \
+	--enable-encoder=aac \
+	--enable-muxer=flac \
+	--enable-muxer=caf \
+	--enable-demuxer=caf \
+	--enable-muxer=aiff \
+	--enable-muxer=mp3 \
+	--enable-demuxer=aac \
+	--enable-demuxer=ac3 \
+	--enable-demuxer=aiff \
+	--enable-demuxer=ape \
+	--enable-demuxer=asf \
+	--enable-demuxer=au \
+	--enable-demuxer=avi \
+	--enable-demuxer=flac \
+	--enable-demuxer=flv \
+	--enable-demuxer=matroska \
+	--enable-demuxer=mov \
+	--enable-demuxer=m4v \
+	--enable-demuxer=mp3 \
+	--enable-demuxer=mpc* \
+	--enable-demuxer=ogg \
+	--enable-demuxer=pcm* \
+	--enable-demuxer=rm \
+	--enable-demuxer=shorten \
+	--enable-demuxer=tak \
+	--enable-demuxer=tta \
+	--enable-demuxer=wav \
+	--enable-demuxer=wv \
+	--enable-demuxer=xwma \
+	--enable-demuxer=dsf \
+	--enable-demuxer=dts \
+	--enable-demuxer=truehd \
+	--enable-decoder=aac* \
+	--enable-decoder=ac3 \
+	--enable-decoder=alac \
+	--enable-decoder=als \
+	--enable-decoder=ape \
+	--enable-decoder=atrac* \
+	--enable-decoder=eac3 \
+	--enable-decoder=flac \
+	--enable-decoder=gsm* \
+	--enable-decoder=mp1* \
+	--enable-decoder=mp2* \
+	--enable-decoder=mp3* \
+	--enable-decoder=mpc* \
+	--enable-decoder=opus \
+	--enable-decoder=ra* \
+	--enable-decoder=ralf \
+	--enable-decoder=shorten \
+	--enable-decoder=tak \
+	--enable-decoder=tta \
+	--enable-decoder=vorbis \
+	--enable-decoder=wavpack \
+	--enable-decoder=wma* \
+	--enable-decoder=pcm* \
+	--enable-decoder=dsd* \
+	--enable-decoder=truehd \
+	--enable-parser=aac* \
+	--enable-parser=ac3 \
+	--enable-parser=cook \
+	--enable-parser=dca \
+	--enable-parser=flac \
+	--enable-parser=gsm \
+	--enable-parser=mpegaudio \
+	--enable-parser=tak \
+	--enable-parser=vorbis
+
 cat ffbuild/config.log
 make
 make install
